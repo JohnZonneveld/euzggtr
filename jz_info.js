0671c0ad52196b65f48f1c451571eea48e104658
John <55062999+raspimeteo@users.noreply.github.com>
2020-03-22 08:27:02 -0500

    Initial upload
    added age id-tage so that page can automatically update age on birthday

---
+++ jz_info.js
@@ -0,0 +1,15 @@
+function calcAge()
+{
+    var dob = '19630930';
+    var year = Number(dob.substr(0, 4));
+    var month = Number(dob.substr(4, 2)) - 1;
+    var day = Number(dob.substr(6, 2));
+    var today = new Date();
+    var age = today.getFullYear() - year;
+    if (today.getMonth() < month || (today.getMonth() == month && today.getDate() < day)) {
+      age--;
+    }
+    return(age);
+}
+document.getElementById("age").innerText = calcAge()
+  
